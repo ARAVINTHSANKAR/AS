775e39143d5cda5c4890f78ae1dc6753328a7dc6
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2017-09-19 00:06:09 +0530

    Update JavaScriptRecapAssignment.js

--- JavaScriptRecapAssignment.js
+++ JavaScriptRecapAssignment.js
@@ -1,6 +1,9 @@
+var id =[8];
+var name =[8];
+var company =[8];
+var country =[8];
 function Jsonstorage1() {
-/* 	      var control = document.getElementById('companyTable');
- control.style.visibility = "visible"; */
+
  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -12,40 +15,34 @@ function Jsonstorage1() {
   var retrievedObject = localStorage.getItem("data");
   var parsedObject = JSON.parse(retrievedObject);
 	document.getElementById("Table").innerHTML = "<tr><th>" +"ID"+ "</th><th>" +"NAME"+ "</th><th>" +"COMPANY"+ "</th><th>" +"COUNTRY"+ "</th><th>" +"RESET"+ "</th></th>";
-  for (var i = 0; i < 8 ; i ++) {
+  for (var i = 0; i < 7 ; i ++) {
 	document.getElementById("Table").innerHTML += "<tr><td>" +parsedObject[i].id+ "</td><td>" +parsedObject[i].name+ "</td><td>" +parsedObject[i].company+ "</td><td>" +parsedObject[i].country+ "</td><td>" +"<button id = mybutt1 onclick = Buttonedit("+i+")>EDIT</button> <button onclick = Buttondelete() >DELETE</button> "+ "</td></tr>";
-
-
+  
+   id[i] = parsedObject[i].id;
+   name[i] = parsedObject[i].name;
+   company[i] = parsedObject[i].company;
+   country[i] = parsedObject[i].country;
+  
   }
-/*     var control = document.getElementById("employeeTable");
- control.style.visibility = "hidden"; */
 }
+
 
 
 function Buttonedit(id) {
 	alert(id);
+	document.getElementById("textid").value = id[id];
+	document.getElementById("demo").value = company[id];
+	document.getElementsByClassName("demo2").innerHTML = country[id];
  	var control = document.getElementById("EditingBox");
 	control.style.visibility = "visible";
-	
 	var x = document.getElementById("mybutt1").id;
 	alert(x);
-	
-	document.getElementById("textid").value = "id";
-	document.getElementById("textname").innerHTML = parsedobject[1].name;
-	document.getElementById("textcompany").innerHTML = parsedObject[1].company;
-	document.getElementById("textcountry").innerHTML = parsedObject[2].country;
-	
-	document.getElementById("demo").value = hello;
 }
 function Buttondelete() {
-	
-
 }
 
 function Jsonstorage2() {
-/* 	    var control = document.getElementById("employeeTable");
- control.style.visibility = "visible"; */
- 
+
  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -60,9 +57,5 @@ function Jsonstorage2() {
   for (var i = 0; i < 8 ; i ++) {
 	document.getElementById("Table").innerHTML += "<tr><td>" +parsedObject[i].id+ "</td><td>" +parsedObject[i].name+ "</td><td>" +parsedObject[i].age+ "</td><td>" +parsedObject[i].contact+ "</td><td>" +"<button>EDIT</button> <button>DELETE</button>"+ "</td></tr>";
   }
-      /* var control = document.getElementById("companyTable");
- control.style.visibility = "hidden"; */
 }
-
-
 
