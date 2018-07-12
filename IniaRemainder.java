a6316d3eafa115a8d4c582a08307d1edc9813ac7
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2018-07-12 22:30:30 +0530

    Add files via upload

--- IniaRemainder.java
+++ IniaRemainder.java
@@ -1,16 +1,33 @@
 package logInia;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public abstract class IniaRemainder {
+public class IniaRemainder {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Aravinth sankar\\Desktop\\geckodriver\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Aravinth sankar\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://sangam.objectfrontier.com/login?service=https%3A%2F%2Finia.objectfrontier.com%2F");
 		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("aravinth.ba");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("qwertyuiop12AS");
+		
+		driver.findElement(By.xpath("//*[@id=\"fm1\"]/button")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"top-menu\"]/ul/li[4]/a")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr[1]/td[5]/a/img")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"wktime_edit\"]/div[2]/a")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"hours4_\"]")).sendKeys("8.00");
+		
+		driver.findElement(By.xpath("//*[@id=\"wktime_save\"]")).click();
 		
 		driver.quit();
 	}
---
+++ runIniaAutomation.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+set CLASSPATH ="C:\JavaWorkspace\logInia\src\logInia".
+javac IniaRemainder.java
+java IniaRemainder
+pause
